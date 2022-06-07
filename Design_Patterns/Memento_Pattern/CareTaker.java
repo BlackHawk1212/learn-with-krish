100201674a38103197d5c9f6e0129cefd6e42ea1
Blackhawk1212 <nimeshmendis1997@gmail.com>
2022-06-07 12:14:08 +0530

    Design Patterns Code Snippet Examples

---
+++ Design_Patterns/Memento_Pattern/CareTaker.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class CareTaker {
+    private final ArrayList<Memento> mementos = new ArrayList<>();
+
+    public void addMemento(Memento memento) {
+        mementos.add(memento);
+    }
+
+    public Memento getMemento() {
+        return mementos.get(1);
+    }
+}
