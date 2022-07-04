a7e3ca1b4b0d9e1e70505e6db777b99d80d0d48c
Blackhawk1212 <nimeshmendis1997@gmail.com>
2022-07-04 13:13:28 +0530

    Did modifications in server.js in HTTP_Keep-Alive

--- MicroService_Demo/HTTP_KeepAlive/server.js
+++ MicroService_Demo/HTTP_KeepAlive/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/wait', async (req, res) => {
+    await new Promise((resolve) => setTimeout(resolve, 60000)) // Set time for 1 Minute
     res.send('Job Done');
 });
 
